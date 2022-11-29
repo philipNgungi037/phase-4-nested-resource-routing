5430a2c4778711f5a1a3014fe5c5291fbbb96630
PHILIP <philophilo037@gmail.com>
2022-11-29 08:31:12 +0300

    completed assignment

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -5,11 +5,16 @@ class DogHousesController < ApplicationController
     dog_house = DogHouse.find(params[:id])
     render json: dog_house
   end
+  def show
+    dog_house = DogHouse.find(params[:id])
+    render json: dog_house
+  end
 
   private
 
   def render_not_found_response
     render json: { error: "Dog house not found" }, status: :not_found
   end
+
 
 end
